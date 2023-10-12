56f1059f91b023224a8087d7e935db663b8e11db
Brendan Malycky <bmalycky@hotmail.com>
2023-10-12 10:26:51 -0600

    created array and objects

---
+++ client.js
@@ -0,0 +1,29 @@
+const characters = ["Legolas", "Aragorn", "Gimli", "Boromir", "Frodo", "Merry", "Sam", "Pippin"];
+
+const smeagol = {
+  name: "smeagol",
+  charType: "antagonist",
+  age: 589,
+  survived: false
+}
+
+const gandalf = {
+  name: "Gandalf",
+  charType: "protagonist",
+  age: 2019,
+  survived: true  
+}
+
+const saruman = {
+  name: "Gandalf",
+  charType: "protagonist",
+  age: 2019,
+  survived: false  
+}
+
+const boromir = {
+  name: "Boromir",
+  charType: "protagonist",
+  age: 40,
+  survived: false 
+}
