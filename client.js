85580e819f6ab1f8619062bed1724e90849183c4
Brendan Malycky <bmalycky@hotmail.com>
2023-10-15 12:26:00 -0600

    completed object methods

--- client.js
+++ client.js
@@ -35,102 +35,145 @@ const objectsArray = [];
 objectsArray.push(smeagol, gandalf, saruman, boromir);
 
 // declare variables //
-let target;
+let targetElement;
 let render;
 
 //******** array methods ********//
 
 // select element to render in //
-target = document.getElementById("array-push");
+targetElement = document.getElementById("array-push");
 // modify with push //
 render = characters.push(saruman);
 // render the methods return value //
-target.innerHTML = render;
+targetElement.innerHTML = render;
 
 // select element to render in //
-target = document.getElementById("array-unshift");
+targetElement = document.getElementById("array-unshift");
 // modify with unshift //
 render = characters.unshift(smeagol);
 // render the methods return value //
-target.innerHTML = render;
+targetElement.innerHTML = render;
 
 
 // select element to render in //
-target = document.getElementById("array-pop");
+targetElement = document.getElementById("array-pop");
 // modify with pop //
 render = characters.pop();
 // render the methods return value.name//
-target.innerHTML = render.name;
+targetElement.innerHTML = render.name;
 
 // select element to render in //
-target = document.getElementById("array-shift");
+targetElement = document.getElementById("array-shift");
 // modify with shift //
 render = characters.shift();
 // render the methods return value.name//
-target.innerHTML = render.name;
+targetElement.innerHTML = render.name;
 
 // select element to render in //
-target = document.getElementById("array-concat");
+targetElement = document.getElementById("array-concat");
 // modify with concat //
 render = characters.concat(objectsArray);
-// render the methods return value.length to show new array length//
-target.innerHTML = render.length;
+// render the methods return value //
+targetElement.innerHTML = render;
 
 // select element to render in //
-target = document.getElementById("array-slice");
+targetElement = document.getElementById("array-slice");
 // modify with slice //
 render = characters.slice(3);
 // render the methods return value //
-target.innerHTML = render;
+targetElement.innerHTML = render;
 
 
 // select element to render in //
-target = document.getElementById("array-splice");
+targetElement = document.getElementById("array-splice");
 // modify with splice //
 render = characters.splice(2, 1);
 // render the methods return value //
-target.innerHTML = render;
+targetElement.innerHTML = render;
 
 // select element to render in //
-target = document.getElementById("array-filter");
+targetElement = document.getElementById("array-filter");
 // search with filter, filtering by data type//
 render = characters.filter( (type) => typeof type === "string");
 // render the methods return value //
-target.innerHTML = render;
+targetElement.innerHTML = render;
 
 // select element to render in //
-target = document.getElementById("array-find");
+targetElement = document.getElementById("array-find");
 // search with find, search for first string with length > 5 //
 render = characters.find( (item) => item.length > 5 );
 // render the methods return value //
-target.innerHTML = render;
+targetElement.innerHTML = render;
 
 // select element to render in //
-target = document.getElementById("array-includes");
+targetElement = document.getElementById("array-includes");
 // search with includes //
 render = characters.includes("orc");
 // render the methods return value //
-target.innerHTML = render;
+targetElement.innerHTML = render;
 
 // select element to render in //
-target = document.getElementById("array-every");
+targetElement = document.getElementById("array-every");
 // search with every //
-render = characters.every( (item) => item.length > 3 );
+render = characters.every( (item) => item.length >= 3 );
 // render the methods return value //
-target.innerHTML = render;
+targetElement.innerHTML = render;
 
 // select element to render in //
-target = document.getElementById("array-some");
+targetElement = document.getElementById("array-some");
 // search with some //
 render = characters.some( (item) => typeof item === "object");
 // render the methods return value //
-target.innerHTML = render;
+targetElement.innerHTML = render;
 
 //******** Object Methods ********//
 
+// select element to render in //
+targetElement = document.getElementById("object-values");
+// get object values //
+render = Object.values(boromir);
+// render the methods return value //
+targetElement.innerHTML = render;
 
+// select element to render in //
+targetElement = document.getElementById("object-keys");
+// get object keys //
+render = Object.keys(boromir);
+// render the methods return value //
+targetElement.innerHTML = render;
 
+// select element to render in //
+targetElement = document.getElementById("object-entries");
+// get keys and values //
+render = Object.entries(gandalf);
+// render the methods return value //
+targetElement.innerHTML = render;
 
+// select element to render in //
+targetElement = document.getElementById("object-freeze");
+// freezes object so cant add new properties or delete existing ones. Existing properties cannot be changed //
+render = Object.freeze(smeagol);
+// render the methods return value.name //
+targetElement.innerHTML = render.name;
 
+// select element to render in //
+targetElement = document.getElementById("object-seal");
+// freezes object cant add new properties or delete existing ones, can only change existing values if writeable //
+render = Object.seal(saruman);
+render.name = "Saruman the white";
+// render the methods return value.name //
+targetElement.innerHTML = render.name;
 
+// select element to render in //
+targetElement = document.getElementById("object-new");
+// create a new date object //
+render = new Date();
+// render the current date //
+targetElement.innerHTML = `${render.getMonth()} ${render.getDate()} ${render.getFullYear()}`;
 
+// select element to render in //
+targetElement = document.getElementById("object-delete");
+// delete a property //
+delete gandalf.age;
+// try to render the deleted properties value //
+targetElement.innerHTML = gandalf.age;
